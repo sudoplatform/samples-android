353dc2e9708bf4d1df7e79ccebd573fc28e10ca8
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2021-03-03 21:10:54 +0000

    Release SiteReputationExample 1.1.0
    null

--- sitereputation/exampleapp/src/androidTest/java/com/sudoplatform/sitereputationexample/explore/ExploreTest.kt
+++ sitereputation/exampleapp/src/androidTest/java/com/sudoplatform/sitereputationexample/explore/ExploreTest.kt
@@ -14,6 +14,7 @@ import com.sudoplatform.sitereputationexample.MainActivity
 import com.sudoplatform.sitereputationexample.settings.settings
 import org.junit.After
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -45,6 +46,7 @@ class ExploreTest {
         Timber.uprootAll()
     }
 
+    @Ignore
     @Test
     fun testUpdate() {
 
@@ -71,6 +73,7 @@ class ExploreTest {
         }
     }
 
+    @Ignore
     @Test
     fun testCheckingOfUrls() {
 
